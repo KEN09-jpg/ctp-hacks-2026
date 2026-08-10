a85aecc86c00c33db7fac38853860e290472b58f
Kendrik <Kendriksanchez13@gmail.com>
2026-08-09 23:07:55 -0400

    remove vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
  
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ctp-hacks-2026/",
   plugins: [react()],
   build: {
     sourcemap: false,
